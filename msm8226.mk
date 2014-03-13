62d4d8f15cc958557a5c6e693b98b4156fd3055c
Satish Kumar Singh <ssing@codeaurora.org>
2014-03-12 20:01:51 -0700

    NFC: Enable Host Card Emulation feature
    Add HCE feature in makefile
    
    Change-Id: I360b0f7880be9589dc705124e71f097266a442fe

--- msm8226.mk
+++ msm8226.mk
@@ -89,7 +89,8 @@ PRODUCT_COPY_FILES += \
         packages/apps/Nfc/migrate_nfc.txt:system/etc/updatecmds/migrate_nfc.txt \
         frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
         frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-        frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
+        frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+        frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
 endif # TARGET_USES_QCA_NFC
 
 PRODUCT_COPY_FILES += \
