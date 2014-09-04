a2f763916a45947f357af98fcc51be7110d0d67e
Suman Mukherjee <sumam@codeaurora.org>
2014-09-04 16:32:35 +0530

    Camera: override camera2.portability.force_api property
    Default API version in L AOSP is 2 and that is causing problem for
    targets which don't support API2. Added this property for targets
    which doesn't support as of now API2.
    
    Change-Id: I5f23950ef987a5b5c20a89a9b303e192319806a9
    CRs-Fixed: 717782

--- msm8226.mk
+++ msm8226.mk
@@ -122,5 +122,8 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.whitelist=/system/etc/whitelist_appops.xml
 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    camera2.portability.force_api=1
+
 PRODUCT_COPY_FILES += \
     device/qcom/msm8226/whitelist_appops.xml:system/etc/whitelist_appops.xml
