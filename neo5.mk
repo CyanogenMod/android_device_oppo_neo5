fdd24b835df8bbfdfbd9aaeab0f0f503d919f381
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-12-15 23:04:42 +1030

    neo5: remove QC base setup. Will be reworked for a cleaner makefile.
    Change-Id: Ic91c50faa9a2981717a41d86509811525a0b7c9e

--- neo5.mk
+++ neo5.mk
@@ -1,22 +1,3 @@
-TARGET_USES_QCOM_BSP := true
-TARGET_USES_QCA_NFC := other
-
-ifeq ($(TARGET_USES_QCOM_BSP), true)
-# Add QC Video Enhancements flag
-TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
-endif #TARGET_USES_QCOM_BSP
-
-DEVICE_PACKAGE_OVERLAYS := device/qcom/msm8226/overlay
-
-#TARGET_DISABLE_DASH := true
-#TARGET_DISABLE_OMX_SECURE_TEST_APP := true
-
-# media_profiles and media_codecs xmls for 8226
-ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS), true)
-PRODUCT_COPY_FILES += device/qcom/msm8226/media/media_profiles_8226.xml:system/etc/media_profiles.xml \
-                      device/qcom/msm8226/media/media_codecs_8226.xml:system/etc/media_codecs.xml
-endif
-
 $(call inherit-product, device/qcom/common/common.mk)
 
 # Audio configuration file
