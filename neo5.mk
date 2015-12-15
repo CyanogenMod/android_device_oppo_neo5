65b4479770c15ba2eab3d8fd630d8ad907156b90
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-12-15 23:15:07 +1030

    neo5: update audio path to fix compile.
    Change-Id: I36807d4e7e628a06953ef169b438be3ccf499e78

--- neo5.mk
+++ neo5.mk
@@ -1,10 +1,29 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 $(call inherit-product, device/qcom/common/common.mk)
 
-# Audio configuration file
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Audio
 PRODUCT_COPY_FILES += \
-    device/qcom/msm8226/audio_policy.conf:system/etc/audio_policy.conf \
-    device/qcom/msm8226/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    device/qcom/msm8226/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 PRODUCT_PACKAGES += \
     libqcomvisualizer \
