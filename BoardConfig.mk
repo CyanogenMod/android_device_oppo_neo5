eeb6576fea279e551fee3f1f734161a823199a2f
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-12-15 21:10:53 +1030

    neo5: SMP is default on Lollipop
    Change-Id: Ib451e40e220196869e80d99b2ef071e75e57139d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,6 @@ TARGET_CPU_ABI  := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := krait
-TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
 TARGET_KERNEL_CONFIG := neo5_defconfig
