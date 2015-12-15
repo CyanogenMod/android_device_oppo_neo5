20becf9773470f7918cea3c9fe89cb7a18834ce2
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-12-16 10:12:47 +1030

    neo5: Add thermal config
    Change-Id: Ic5c6a17fe6fa7479afbbe15abbf752c25459abb5

--- neo5.mk
+++ neo5.mk
@@ -117,5 +117,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
+# Thermal config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine-8226.conf
+
 # Call the proprietary setup
 $(call inherit-product-if-exists, vendor/oppo/neo5/neo5-vendor.mk)
