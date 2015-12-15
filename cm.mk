8fe56aac0f4efbfed2f1f923f19ddcf699cd1f99
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-12-15 20:06:08 +1030

    neo5: add CM makefile.
    Change-Id: I622efeac7080d48b43ae114e98233bfb2eb2a155

---
+++ cm.mk
@@ -0,0 +1,35 @@
+# Copyright (C) 2014-2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, device/oppo/neo5/neo5.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+PRODUCT_NAME := cm_neo5
+PRODUCT_DEVICE := neo5
+PRODUCT_BRAND := OPPO
+PRODUCT_MANUFACTURER := OPPO
+PRODUCT_MODEL := Neo5
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=4.3/JLS36C/1390465867:user/release-keys \
+    PRIVATE_BUILD_DESC="OPPO/R831L/R831L:4.3/JLS36C/1401246782:user/release-keys" \
+    TARGET_DEVICE="NEO5"
