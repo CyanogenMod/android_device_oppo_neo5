669de56892e8afb4b89f8bed0701a8c03222b700
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-12-15 22:57:21 +1030

    neo5: remove yet another define we have already
    We have this in the cm.mk so this can go. Thanks anyway CAF!
    
    Change-Id: Ide0f3c44ee091611236d89fe8474cd4dd3823aa3

--- neo5.mk
+++ neo5.mk
@@ -19,9 +19,6 @@ endif
 
 $(call inherit-product, device/qcom/common/common.mk)
 
-PRODUCT_NAME := msm8226
-PRODUCT_DEVICE := msm8226
-
 # Audio configuration file
 PRODUCT_COPY_FILES += \
     device/qcom/msm8226/audio_policy.conf:system/etc/audio_policy.conf \
